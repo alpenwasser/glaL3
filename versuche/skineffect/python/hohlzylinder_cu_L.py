27f99253fd5deb9f9b3c0e932aa14832f28cbb0a
alpenwasser <webmaster@alpenwasser.net>
2015-10-30 13:44:43 +0100

    skin effect: hohlzylinder_cu_L.py: cleanup stage 1

--- versuche/skineffect/python/hohlzylinder_cu_L.py
+++ versuche/skineffect/python/hohlzylinder_cu_L.py
@@ -6,72 +6,73 @@ from sympy.external import import_module
 from mpmath import *
 from matplotlib.pyplot import *
 from math import copysign
-
 init_printing()
 
-#mp.dps=27  # precision in decimal places, prec ~= 3.33*dps
-#mp.prec=80 # precision in bits
-mp.prec=64 # precision in bits
 
-# ---------------------------------------------------------#
-# Measurement in Center, depending on Frequency            #
-# ---------------------------------------------------------#
+# ************************************************************************** #
+# Self-Inductance L of copper coil with hollow copper cylinder inserted.     #
+# ************************************************************************** #
+
+# All values are in standard SI units unless otherwise noted.
+
+# -------------------------------------------------------- #
+# Default precision is insufficient, therefore we increase #
+# precision.   One  can  increase the  number  of  decimal #
+# places or bits, where the number of bits places is ~3.33 #
+# times the number of decimal places.                      #
+# -------------------------------------------------------- #
+#mp.dps=25  # decimal places
+mp.prec=80 # precision in bits
 
 
 # ---------------------------------------------------------#
 # Init, Define Variables and Constants                     #
 # ---------------------------------------------------------#
-var('mu0 B_abs B_arg B B0 j0 k r r0 w f sigma denom enum')
-mu0   = 4*pi*1e-7
-#sigma = 58.1e6                  # de.wikipedia.org/wiki/Kupfer
-sigma = 52e6
-r     = 30e-3
+var('mu0 B_abs B_arg B B0 j0 k r w f sigma denom enum')
+mu0   = 4*pi*1e-7                                        # vacuum permeability
+sigma = 52e6                            # de.wikipedia.org/wiki/Kupfer: 58.1e6
+r     = 0             # radial position of measurement probe. Centered on axis
 dsp   = 98e-3                                               # diameter of coil
-rsp   = dsp / 2
-r0    = 45e-3
-r1    = 30e-3
-r2    = 35e-3
+rsp   = dsp / 2                                               # radius of coil
+r1    = 30e-3                                # inner radius of copper cylinder
+r2    = 35e-3                                # outer radius of copper cylinder
 B0    = 6.9e-2                             # adjust this as needed for scaling
-N0    = 574
-l     = 500e-3
+N0    = 574                                   # number of turns of copper coil
+l     = 500e-3                                         # length of copper coil
 
 
 # ---------------------------------------------------------#
 # Function for magnetic Field B                            #
+#                                                          #
+# See formula 28 on p.15 of script for experiment.         #
+#                                                          #
+# NOTE: We use  frequency f  instead of  angular frequency #
+# omega since that is what we actually set on the function #
+# generator.                                               #
 # ---------------------------------------------------------#
-# See formula 21 on p.11 of script for experiment.
 
-# TODO: Switch from w to f!!!!!
+k = lambda f: sqrt((2*np.pi*f*mu0*sigma)/2)*(mpc(1,-1))          # degrees/sec
 
-#k = lambda w: sqrt((w*mu0*sigma)/2)*(mpc(1,-1))                     # rad/sec
-k = lambda w: sqrt((2*np.pi*w*mu0*sigma)/2)*(mpc(1,-1))          # degrees/sec
-
-# Enumerator:
-enum  = lambda w: besselj(0,k(w)*r)  * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(0,k(w)*r)
-denom = lambda w: besselj(0,k(w)*r2) * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(0,k(w)*r2)
-
-B = lambda w: enum(w) / denom(w) * B0
-
-enum1     = lambda w: besselj(0,k(w)*r1) * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(0,k(w) * r1)
-denom1    = lambda w: besselj(0,k(w)*r2) * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(0,k(w) * r2)
-enum2     = lambda w: r2 * (besselj(1,k(w)*r2) * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(1,k(w) * r2)) - r1 * (besselj(1,k(w)*r1) * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(1,k(w) * r1))
-denom2    = lambda w: besselj(0,k(w)*r2) * bessely(2,k(w)*r1) - besselj(2,k(w)*r1) * bessely(0,k(w) * r2)
+enum1 = lambda f: besselj(0,k(f)*r1) * bessely(2,k(f)*r1) - besselj(2,k(f)*r1) * bessely(0,k(f) * r1)
+denom1    = lambda f: besselj(0,k(f)*r2) * bessely(2,k(f)*r1) - besselj(2,k(f)*r1) * bessely(0,k(f) * r2)
+enum2     = lambda f: r2 * (besselj(1,k(f)*r2) * bessely(2,k(f)*r1) - besselj(2,k(f)*r1) * bessely(1,k(f) * r2)) - r1 * (besselj(1,k(f)*r1) * bessely(2,k(f)*r1) - besselj(2,k(f)*r1) * bessely(1,k(f) * r1))
+denom2    = lambda f: besselj(0,k(f)*r2) * bessely(2,k(f)*r1) - besselj(2,k(f)*r1) * bessely(0,k(f) * r2)
 term3     = rsp ** 2 - r2**2
 prefactor = mu0 * pi * N0**2 / l
 
-phi_norm = lambda w: prefactor * (r1**2 * enum1(w)/denom1(w) + 2/k(w) * enum2(w)/denom2(w) + term3)
+phi_norm = lambda f: prefactor * (r1**2 * enum1(f)/denom1(f) + 2/k(f) * enum2(f)/denom2(f) + term3)
 
-L = lambda w: re(phi_norm(w))
+L = lambda f: re(phi_norm(f))
+
 
 # ---------------------------------------------------------#
 # Generate points for omega axis                           #
 # ---------------------------------------------------------#
-var('npts expufunc n B_w Bw Babs Barg fmax fmin')
-npts = 1e2
+npts = 1e3
 fmin=1
-#fmax=1943 # maximum calculable frequency
+#fmax=1943 # maximum calculable frequency for default precision
 #fmax = 2500
-fmax = 1500
+fmax = 2500
 n = np.linspace(1,npts,npts)
 expufunc = np.frompyfunc(exp,1,1)
 frequency_vector = 1*expufunc(n*log(fmax-1)/npts)
@@ -81,11 +82,6 @@ frequency_vector = 1*expufunc(n*log(fmax-1)/npts)
 L_ufunc = np.frompyfunc(L,1,1)
 L_num   = L_ufunc(frequency_vector)
 
-# ---------------------------------------------------------#
-# Measurement Values from the actual experiment            #
-# ---------------------------------------------------------#
-frequencies = np.array([    1,     10,      20,      40,      80,     120,     160,   200,    400,    600,    800,   1000, 1200, 1500])
-L_measured  = L_ufunc(frequencies)
 
 # ---------------------------------------------------------#
 # Plot the Things                                          #
@@ -98,20 +94,11 @@ font = {
         'size'   : 16,
         }
 
-#subplot(2,1,1)
 plot(frequency_vector,L_num,color='blue',label='Fitfunktion')
-scatter(frequencies,L_measured,color='black',s=64,label='Messwerte')
+#scatter(frequencies,L_measured,color='black',s=64,label='Messwerte')
 #xlabel('Frequenz (Hz)',fontdict=font)
 #ylabel('Spannung (Volt)',fontdict=font)
 #title('Betrag des Magnetfelds in Zylinderspule mit Hohlzylinder aus Kupfer, (Messpunkt: auf Zylinderachse, horizontal zentriert)',fontdict=font)
 #legend(fontsize=16)
 xscale('log')
-#subplot(2,1,2)
-#plot(frequency_vector,phi_norm_arg_num,color='blue',label='Fitfunktion')
-#scatter(frequencies,phi_norm_arg_measured,color='black',s=64,label='Messwerte')
-#xlabel('Frequenz (Hz)',fontdict=font)
-#ylabel('Phase (Grad))',fontdict=font)
-#title('Phase des Magnetfelds in Zylinderspule mit Hohlzylinder aus Kupfer (Messpunkt: auf Zylinderachse, horizontal zentriert)',fontdict=font)
-#legend(fontsize=16)
-#xscale('log')
 show()
