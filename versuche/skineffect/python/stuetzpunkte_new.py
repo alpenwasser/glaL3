e4ef4f07e78a6a4bea02df5e504ddd5becbcd9bc
alpenwasser <webmaster@alpenwasser.net>
2015-10-31 14:50:26 +0100

    skin effect: stuetzpunkte.py: polishing

--- versuche/skineffect/python/stuetzpunkte_new.py
+++ versuche/skineffect/python/stuetzpunkte_new.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from sympy import *
-from sympy.external import import_module
 from mpmath import *
 from matplotlib.pyplot import *
 #init_printing()     # make things prettier when we print stuff for debugging.
@@ -17,10 +16,12 @@ from matplotlib.pyplot import *
 # ---------------------------------------------------------#
 # Define Variables and Constants                           #
 # ---------------------------------------------------------#
-npts = 5e1 # careful: number of points is npts + 1 (starts at 0)
-fmin = 8e1
-fmax = 5e4
-#fmax = 5e3
+npts              = 5e1 # careful: number of points is npts + 1 (starts at 0)
+fmin              = 8e1
+fmax              = 5e4
+highest_frequency = fmin * exp(log(fmax-fmin))
+freq_max_ratio    = highest_frequency / fmax
+#fmax              = 5e3
 font = {
         'family' : 'serif',
         'color'  : 'black',
@@ -31,16 +32,17 @@ plot_legend_fontsize       = 16
 plot_color_old             = 'magenta'
 plot_color_new             = 'blue'
 plot_color_common          = 'black'
-plot_label_points_old      = r"St\"utzpunktformel: $\displaystyle f_k = f_{min} \cdot exp\Biggr(\frac{k}{NPTS} \cdot ln(f_{max}-f_{min})\Biggr)$"
-plot_label_points_new      = r"St\"utzpunktformel: $\displaystyle f_k = exp\Biggr((1-\frac{k}{NPTS}) \cdot ln(f_{min})\Biggr) \cdot exp\Biggr(\frac{k}{NPTS} \cdot ln(f_{max})\Biggr)$"
+plot_label_points_old      = r"St\"utzpunktformel A: $\displaystyle f_{kA} = f_{min} \cdot exp\Biggr(\frac{k}{NPTS} \cdot ln(f_{max}-f_{min})\Biggr)$"
+plot_label_points_new      = r"St\"utzpunktformel B: $\displaystyle f_{kB} = exp\Biggr((1-\frac{k}{NPTS}) \cdot ln(f_{min})\Biggr) \cdot exp\Biggr(\frac{k}{NPTS} \cdot ln(f_{max})\Biggr)$"
 plot_label_vertical_common = r"minimale Frequenz St\"utzpunkt: "
 plot_label_vertical_old    = r"maximale Frequenz St\"utzpunkt, Methode A: "
 plot_label_vertical_new    = r"maximale Frequenz St\"utzpunkt, Methode B: "
+plot_added_text            = r"Verh\"altnis der maximalen Frequenzen Methode A -- Methode B: $\displaystyle \frac{f_{kA}}{f_{kB}}\Bigg|_{k=NPTS} \approx " + str(int(freq_max_ratio)) + "$"
 plot_size_measurements     = 64
 plot_scale_x               = 'log'
 plot_label_x               = r"Frequenz des St\"utzpunkts (Hz)"
 plot_1_label_y             = 'k (siehe Formel)'
-plot_1_title               = r"Vergleich St\"utzpunktformeln f\"ur den Frequenzbereich von " + str(fmin) + " Hz bis " + str(fmin * exp(log(fmax-fmin))/1000) + " kHz, " + str(int(npts+1)) + " Punkte"
+plot_1_title               = r"Vergleich St\"utzpunktformeln f\"ur den Frequenzbereich von " + str(fmin) + " Hz bis " + str(highest_frequency) + " Hz, " + str(int(npts+1)) + " Punkte"
 y_lim_low                  = -2
 y_lim_high                 = npts + 2
 x_lim_low                  = 0.67 * fmin
@@ -64,7 +66,6 @@ plot_label_vertical_new    += str(frequency_vector_new[npts-1]) + " Hz"
 # Plot the Things                                          #
 # ---------------------------------------------------------#
 
-# Figure 1: Range includes all measurement values.
 fig1 = figure(1)
 axes1 = fig1.add_subplot(111)
 matplotlib.pyplot.rc('text', usetex=True)
@@ -108,6 +109,22 @@ axes1.set_xlim([x_lim_low,x_lim_high])
 axes1.set_xlabel(plot_label_x,fontdict=font)
 axes1.set_ylabel(plot_1_label_y,fontdict=font)
 axes1.set_title(plot_1_title,fontdict=font)
-axes1.legend(fontsize=plot_legend_fontsize,loc='upper left')
+
+    # ---------------------------------------------------- #
+    # Work some  magic to append  the fraction of  the two #
+    # methods  to  the legend  instead  of  it being  some #
+    # random piece of text on the plot.                    #
+    # ---------------------------------------------------- #
+rect = matplotlib.patches.Rectangle([0,0],0,0,color='white',label=plot_added_text)
+handles,legends = axes1.get_legend_handles_labels()
+handles.append(rect)
+axes1.legend(handles=handles,fontsize=plot_legend_fontsize,loc='upper left')
+
+    # ---------------------------------------------------- #
+    # This  would be  necessary if  we wanted  to actually #
+    # draw  the  patch,  leaving   this  here  for  future #
+    # reference.                                           #
+    # ---------------------------------------------------- #
+#axes1.add_patch(rect)
 
 show()
